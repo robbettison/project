0855206776c9b78e0685bb23fbe3de280312d6ba
Rob Bettison <rob.bettison94@gmail.com>
2017-02-20 16:46:43 +0000

    update character

--- character.java
+++ character.java
@@ -1,6 +1,27 @@
-import java.swing.*;
+package groupproject;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
-public class Character extends JFrame {
-  JButton character = new JButton("bye");
-}ijnfrsijnwr
+public class character extends JFrame{
+  JButton character = new JButton("kev");
+  public character() {
+	  character.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	});
+	 add(character);
+     setVisible(true);
+  }
+  
+  public static void main(String[] args) {
+	character aCharacter = new character();
+	
+  }
+}
