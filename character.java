98bd83a6b59392815a261cc25d3fe12cc858d574
Rob Bettison <rob.bettison94@gmail.com>
2017-02-21 12:25:31 +0000

    test

--- character.java
+++ character.java
@@ -4,24 +4,25 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-
+HELLO ZHONG
 public class character extends JFrame{
   JButton character = new JButton("kev");
   public character() {
 	  character.addActionListener(new ActionListener() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
 	});
 	 add(character);
+	 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
      setVisible(true);
   }
-  
+
   public static void main(String[] args) {
 	character aCharacter = new character();
-	
+
   }
 }
