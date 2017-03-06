c98765a417e6acfb886cf6b2c38d78fd240bd94f
Hugo Keung <hugokeung@gmail.com>
2017-03-06 16:04:32 +0000

    with road pic

--- keyboardevent2.java
+++ keyboardevent2.java
@@ -13,19 +13,23 @@ import javafx.scene.input.KeyCode;
 public class keyboardevent2 extends Application {
   private int n = 0;
   int x=175;
+int channel = 1;
+
   Canvas canvas = new Canvas(400, 300);
+Image bg = new Image("road.jpeg");
   GraphicsContext g = canvas.getGraphicsContext2D();
 
   public void start (Stage stage) {
     Group root = new Group(canvas);
     Scene road = new Scene(root);
     stage.setScene(road);
-    draw(g, x);
+    draw(g, x, bg);
     stage.show();
     road.setOnKeyPressed(this::handle);
   }
 
-  private void draw(GraphicsContext g, int x) {
+  private void draw(GraphicsContext g, int x, Image bg) {
+g.drawImage(bg, 0, 0);
     g.fillOval(x, 175, 50, 50);
   }
 
@@ -34,15 +38,22 @@ public class keyboardevent2 extends Application {
   public void handle(KeyEvent event) {
     if(event.getCode() == KeyCode.RIGHT) {
       System.out.println("RIGHT");
-      x+=50;
-      g.clearRect(0, 0, 400, 300);
-      draw(g, x);
+     
+if (channel < 4){
+channel +=1;
+ x+=50;
+
+      draw(g, x, bg);
+}
     }
     else if(event.getCode() == KeyCode.LEFT) {
       System.out.println("LEFT");
+if (channel >0){
+channel -=1;
       x-=50;
-      g.clearRect(0, 0, 400, 300);
-      draw(g, x);
+
+      draw(g, x, bg);
+}
     }
   }
 }
